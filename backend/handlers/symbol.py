2f5e416252bcacf57b40f6522f325be719c62958
rajeevrj256 <rjrajeev5918@gmail.com>
2024-08-29 00:58:34 +0530

    Update symbol.py

--- backend/handlers/symbol.py
+++ backend/handlers/symbol.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
 import requests
+import logging
+
 
 get_symbols_blueprint = Blueprint('get_symbols', __name__)
 
@@ -11,5 +13,11 @@ def get_symbols():
     
     response = requests.get('https://api.binance.com/api/v3/exchangeInfo')
     data = response.json()
-    symbols_list = [symbol['symbol'] for symbol in data['symbol']]
-    return jsonify(symbols_list), 200
+    logging.info(f"API Response: {data}")
+
+    if 'symbols' in data:
+        symbols_list = [symbol['symbol'] for symbol in data['symbols']]
+        return jsonify(symbols_list), 200
+    else:
+        logging.error("The 'symbols' key is missing from the response.")
+        return jsonify({"error": "API response does not contain 'symbols'"}), 500
