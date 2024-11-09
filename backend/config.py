8854fa8c0a535f988ec23c0ce2e9f0db9bf3723e
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-11-09 19:31:49 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 # Configuration settings
 JWT_SECRET_KEY = 'your_jwt_secret_key'
-MONGO_URI = 'mongodb+srv://sorajeev5918:g3u9NH3VKJ45yTKw@cluster0.egtqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+MONGO_URI = 'mongodb+srv://sorajeev591=majority&appName=Cluster0'
